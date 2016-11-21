18e1d793195d60ae1a6e4a3763b60c0a24f14c7d
Azrael666 <di_teschner@yahoo.de>
2016-11-21 11:05:39 +0100

    Put drawData Method in DataObject Class

--- Intelligente Systeme/src/aufgabe_2/DataObject.java
+++ Intelligente Systeme/src/aufgabe_2/DataObject.java
@@ -6,6 +6,17 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.io.LineNumberReader;
 
+import javax.imageio.ImageIO;
+
+import org.jzy3d.chart.Chart;
+import org.jzy3d.chart.ChartLauncher;
+import org.jzy3d.colors.Color;
+import org.jzy3d.colors.ColorMapper;
+import org.jzy3d.colors.colormaps.ColorMapRainbow;
+import org.jzy3d.maths.Coord2d;
+import org.jzy3d.maths.Coord3d;
+import org.jzy3d.plot3d.primitives.MultiColorScatter;
+
 public class DataObject {
 	double[][] values;
 	double[] colAverage;
@@ -13,6 +24,7 @@ public class DataObject {
 	double[] colMaximum;
 	int rows = 0;
 	int cols = 0;
+	int size = 0;
 	
 	int valueCount = 0;
 	double valueMin = Double.MAX_VALUE;
@@ -38,6 +50,8 @@ public class DataObject {
 			String firstLine = input.readLine();
 			String[] elementCount = firstLine.split(",");
 			cols = elementCount.length;
+			
+			size = rows * cols;
 			
 			values = new double[rows][cols];
 			colAverage = new double[cols];
@@ -81,6 +95,58 @@ public class DataObject {
 				colMinimum[i] = colMin;
 				colMaximum[i] = colMax;
 			}
+	}
+	
+	public void draw(boolean safeImages) {
+		
+		int x;
+		int y;
+		double z;
+		Coord3d[] points = new Coord3d[size];
+
+		int coordCounter = 0;
+		
+		// Create scatter points
+		for(int i=0; i<rows; i++){
+			for(int j = 0; j < cols; j++) {
+				x = i;
+			    y = j;
+			    z = values[i][j];
+			    points[coordCounter] = new Coord3d(x, y, z);
+			    coordCounter++;
+			}
+		    
+		}
+		
+		// Create a drawable scatter with a colormap
+		ColorMapRainbow colorMapRainbow = new ColorMapRainbow();
+		ColorMapper colorMapper = new ColorMapper(colorMapRainbow , (int)valueMin, (int)valueMax);
+		MultiColorScatter scatter = new MultiColorScatter( points, colorMapper);
+		
+		// Create a chart and add scatter
+		Chart chart = new Chart();
+		chart.getAxeLayout().setMainColor(Color.WHITE);
+		chart.getView().setBackgroundColor(Color.BLACK);
+		chart.getScene().add(scatter);
+		chart.getView().rotate(new Coord2d(0.0, -0.5), true);
+		ChartLauncher.openChart(chart);
+		
+		// Safe Images
+		if(safeImages) {
+			for(int i = 0; i < 63; i++) {
+				Coord2d rotateCoord= new Coord2d(0.1, 0.0);
+				chart.getView().rotate(rotateCoord, true);
+				
+				File outputfile = new File("Images\\Data0\\image" + i + ".jpg");
+				try {
+					ImageIO.write(chart.screenshot(), "jpg", outputfile);
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+//		System.out.println("Barycentre: " + scatter.getBarycentre());
+		
 	}
 	
 }
--- Intelligente Systeme/src/aufgabe_2/Main.java
+++ Intelligente Systeme/src/aufgabe_2/Main.java
@@ -1,20 +1,6 @@
 package aufgabe_2;
 
 import java.awt.Point;
-import java.io.File;
-import java.io.IOException;
-
-import javax.imageio.ImageIO;
-
-import org.jzy3d.chart.Chart;
-import org.jzy3d.chart.ChartLauncher;
-import org.jzy3d.colors.Color;
-import org.jzy3d.colors.ColorMapper;
-import org.jzy3d.colors.colormaps.ColorMapRainbow;
-import org.jzy3d.maths.Coord2d;
-import org.jzy3d.maths.Coord3d;
-import org.jzy3d.plot3d.primitives.MultiColorScatter;
-
 
 public class Main {
 	
@@ -24,61 +10,6 @@ public class Main {
 	static private String dataFileName = "data0.csv";
 	static private String labelFileName = "label0.csv";
 	static private boolean safeImages = false;
-
-	
-	private static void drawData(double[][] array, int rows, int cols, double minValue, double maxValue) {
-				
-		int size = rows * cols;
-		
-		int x;
-		int y;
-		double z;
-		Coord3d[] points = new Coord3d[size];
-
-		int coordCounter = 0;
-		
-		// Create scatter points
-		for(int i=0; i<rows; i++){
-			for(int j = 0; j < cols; j++) {
-				x = i;
-			    y = j;
-			    z = array[i][j];
-			    points[coordCounter] = new Coord3d(x, y, z);
-			    coordCounter++;
-			}
-		    
-		}
-		
-		// Create a drawable scatter with a colormap
-		ColorMapRainbow colorMapRainbow = new ColorMapRainbow();
-		ColorMapper colorMapper = new ColorMapper(colorMapRainbow , (int)minValue, (int)maxValue);
-		MultiColorScatter scatter = new MultiColorScatter( points, colorMapper);
-		
-		// Create a chart and add scatter
-		Chart chart = new Chart();
-		chart.getAxeLayout().setMainColor(Color.WHITE);
-		chart.getView().setBackgroundColor(Color.BLACK);
-		chart.getScene().add(scatter);
-		chart.getView().rotate(new Coord2d(0.0, -0.5), true);
-		ChartLauncher.openChart(chart);
-		
-		// Safe Images
-		if(safeImages) {
-			for(int i = 0; i < 63; i++) {
-				Coord2d rotateCoord= new Coord2d(0.1, 0.0);
-				chart.getView().rotate(rotateCoord, true);
-				
-				File outputfile = new File("Images\\Data0\\image" + i + ".jpg");
-				try {
-					ImageIO.write(chart.screenshot(), "jpg", outputfile);
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
-		}
-//		System.out.println("Barycentre: " + scatter.getBarycentre());
-		
-	}
 	
 	public static void main(String[] args) {
 		
@@ -87,7 +18,7 @@ public class Main {
 		DataObject data = new DataObject(dataFileName);
 		LabelObject label = new LabelObject(labelFileName);
 		
-		drawData(data.values, data.rows, data.cols, data.valueMin, data.valueMax);
+		data.draw(safeImages);
 		
 		long endTime = System.currentTimeMillis();
 		long computingTime = endTime - startTime;
